3512936b40af4eccdca0f07c132bbadfa2faaea9
josephtveter <josephtveter@gmail.com>
2016-12-10 14:41:13 -0700

    Some changes

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.fracturednations.com/opencart/admin/');
+define('HTTP_CATALOG', 'http://www.fracturednations.com/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.fracturednations.com/opencart/admin/');
+define('HTTPS_CATALOG', 'http://www.fracturednations.com/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/fn/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/html/fn/opencart/system/');
+define('DIR_IMAGE', '/var/www/html/fn/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/html/fn/opencart/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/fn/opencart/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/fn/opencart/system/config/');
+define('DIR_CACHE', '/var/www/html/fn/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/fn/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/fn/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/fn/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/fn/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/html/fn/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Flubsy42`');
+define('DB_DATABASE', 'witness_music');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.fracturednations.com/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.fracturednations.com/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/fn/opencart/catalog/');
+define('DIR_SYSTEM', '/var/www/html/fn/opencart/system/');
+define('DIR_IMAGE', '/var/www/html/fn/opencart/image/');
+define('DIR_LANGUAGE', '/var/www/html/fn/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/fn/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/fn/opencart/system/config/');
+define('DIR_CACHE', '/var/www/html/fn/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/fn/opencart/system/storage/download/');
+define('DIR_LOGS', '/var/www/html/fn/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/html/fn/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/html/fn/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Flubsy42`');
+define('DB_DATABASE', 'witness_music');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
